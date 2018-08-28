c523fd31cda11ac244961a153178109dbacc200f
流年 <964652891@qq.com>
2018-08-28 14:50:23 +0800

    orign

--- ex17-20/ex17_20.py
+++ ex17-20/ex17_20.py
@@ -1,1 +1,19 @@
+file_name = input("File name:\n> ")
+print(f"\nCreating your file \'{file_name}\'....\n")
+txt = open(file_name,'w+')
 
+line1 = 'I am a superman.'
+line2 = 'No blood, no revolution.'
+line3 = 'He, the one who changed the world.'
+
+def wline(a,n):
+    a.write(eval('line'+str(n))+'  length='+str(len(eval('line'+str(n)))))
+    a.write("\n")
+
+for i in range(1,4):
+    wline(txt,i)
+
+txt.seek(0)
+
+print(f"{file_name}:")
+print(txt.read())
